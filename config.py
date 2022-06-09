5e4af4c40f615d0053f040d8ae92e571ddb063e0
Sergio Brenes <sergio_brenes@hakkoda.io>
2022-06-08 23:39:12 -0600

    Loading project files

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+snowflake_conn_prop = {
+   "account": os.environ.get("SNOW_ACC"),
+   "user": os.environ.get("SNOW_USER"),
+   "password": os.environ.get("SNOW_PASS")
+ 
+}
+
+# Snowflake Account Identifiers
+# https://docs.snowflake.com/en/user-guide/admin-account-identifier.html#account-identifier-formats-by-cloud-platform-and-region
+#  
